4903ae851711d1f6e90253260e42777aa532eebb
Yingkai Liu <water@mail.nankai.edu.cn>
2018-11-14 09:10:58 +0800

    autohide topnav

--- assets/js/PostJS.js
+++ assets/js/PostJS.js
@@ -24,4 +24,17 @@
         x.style.display = "none";
         document.getElementById("main").style.marginLeft = "6%";
     }
+	
+	/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+	  var currentScrollPos = window.pageYOffset;
+	  if (prevScrollpos > currentScrollPos) {
+		document.getElementById("topnav").style.top = "0";
+	  } else {
+		document.getElementById("topnav").style.top = "-50px";
+	  }
+	  prevScrollpos = currentScrollPos;
+	}
+		
 }
